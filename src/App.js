bc98f4db52c74835c315bd915a6e3cf12dd44521
dalton salmonjr <daltons09@gmail.com>
2019-09-06 00:50:39 -0400

    code clean up and minor css changes in positioning

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 
+const styles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  }
+})
+)
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
      weight: 0,
      height: 0,
-     bmi: 0,
+     bmi: '',
      bmiClass: ''
     };
  this.handleWeightChange = this.handleWeightChange.bind(this); 
@@ -20,6 +27,7 @@ export default class App extends React.Component {
  this.getBMI = this.getBMI.bind(this);
  this.reset = this.reset.bind(this);
  this.bmiZone = this.bmiZone.bind(this);
+ this.getBMIClass = this.getBMIClass.bind(this);
  }
 
 handleWeightChange(event) {
@@ -31,9 +39,11 @@ handleWeightChange(event) {
 }
 
 getBMI() {
-  this.setState({bmi: ((this.state.weight  * 703) / (this.state.height ** 2)).toFixed(1) })
-  
-  this.setState({bmiClass: this.bmiZone(this.state.bmi)})
+  this.setState({bmi: ((this.state.weight  * 703) / (this.state.height ** 2)).toFixed(1) });
+}
+
+getBMIClass() {
+  this.setState({bmiClass: this.bmiZone(this.state.bmi) });
 }
 
 bmiZone(bmi) {
@@ -43,27 +53,27 @@ bmiZone(bmi) {
   else if (bmi >= 18.5 && bmi <= 24.9) {
     return "Optimal Weight";
   }
-  else if (this.state.bmi >= 25 && bmi <= 29.9) {
+  else if (bmi >= 25 && bmi <= 29.9) {
     return "Overweight"
   }
   else if (bmi >= 30) {
     return "Obesity"
     }
-  else {
-    return "nothin"
-  }
 }
 
 reset(){
-  this.setState({ weight: 0, height: 0 , bmi: 0, bmiClass: 'nothin'})
+  this.setState({ weight: 0, height: 0 , bmi: '', bmiClass: ''})
 }  
+
+
 
 render() {
   return (
     <div className="App">
-    <h1 className="App-title"> My BMI? </h1>
+    <h1 className="App-title"> My BMI </h1>
    
-    <ul>
+    
+    
      <label>
       Weight:
         <input 
@@ -72,9 +82,11 @@ render() {
         onChange={this.handleWeightChange}
         value={this.state.weight}  />
       </label>
-      </ul>
-     
-      <ul>
+      
+      
+  
+      
+      
       <label>
       Height:
         <input 
@@ -83,26 +95,32 @@ render() {
         onChange={this.handleHeightChange}
         value={this.state.height}   />
       </label>
-       </ul>  
         
-        <ul> 
-        <Button  className='btn-calc' onClick={this.getBMI}>
-        Get Bmi
+        
+        
+        <Button className='bmi-calc' onClick={this.getBMI}>
+        Get BMI
         </Button>
-        </ul>
+      
         
-        <ul>
+        
+        <Button className='bmi-class' onClick={this.getBMIClass}>
+        BMI Class + Info 
+        </Button>
+        
+        
+        
         <Button  className='reset' onClick={this.reset}>
         Reset
         </Button>
-        </ul>
+        
         
     
     
-    <div className="row">
+    
       <h3>BMI = {this.state.bmi}</h3>
-      <h5>BMI Class = {this.state.bmiClass}</h5>
-    </div>
+      <h4>BMI Class = {this.state.bmiClass}</h4>
+    
     
     
     
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import About from './components/About.js';
+
+export default class About extends React.Component {
+	
+}
+
+render() {
+	
+}
